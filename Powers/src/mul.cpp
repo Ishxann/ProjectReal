c20fd6c4aa4b1ac4f8bb4b8ac92954a69a3aceaa
Ishxann <ishxan.grigoryan09@gmail.com>
2025-04-16 16:11:06 +0000

    Power2

--- Powers/src/mul.cpp
+++ Powers/src/mul.cpp
@@ -1,9 +1,21 @@
+#include <stdexcept>
+
 double power(int x, int n) {
     if (n == 0) return 1.0;
-    if (n < 0) return 0.0;
+    if (x == 0 && n < 0) return 0;
     double result = 1.0;
-    for (int i = 1; i <= n; i++) {
+    int count = n;
+    if (n < 0) {
+        count = -n;
+    }
+    
+    for (int i = 1; i <= count; i++) {
         result = result * x;
     }
+    
+    if (n < 0) {
+        result = 1.0 / result;
+    }
+    
     return result;
 }
--- Powers/tests/main.cpp
+++ Powers/tests/main.cpp
@@ -1,26 +1,33 @@
 #include "mul.h"
 #include <gtest/gtest.h>
 
-TEST(PowerTest, ZeroExponent) {
+TEST(PowerTest, Empty) {
     EXPECT_EQ(power(5, 0), 1.0);
 }
-
+TEST(PowerTest, ZERO) {
+   EXPECT_EQ(power(0, 0), 1.0);
+}
+TEST(PowerTest, ZeroBase) {
+   EXPECT_EQ(power(0, 5), 0.0);
+}
 TEST(PowerTest, ExponentOne) {
     EXPECT_EQ(power(3, 1), 3.0);
 }
-
 TEST(PowerTest, PositiveExponent) {
     EXPECT_EQ(power(2, 3), 8.0);
 }
-
 TEST(PowerTest, NegativeBase) {
     EXPECT_EQ(power(-2, 3), -8.0);
 }
-
-TEST(PowerTest, ZeroBase) {
-    EXPECT_EQ(power(0, 5), 0.0);
-}
-
 TEST(PowerTest, NegativeExponent) {
-    EXPECT_EQ(power(2, -2), 0.0);
+    EXPECT_EQ(power(2, -2), 0.25);
+}
+TEST(PowerTest, NegativeBase2) {
+   EXPECT_EQ(power(-2, 2), 4);
+}
+TEST(PowerTest, NegativeBaseNegativeExponent) {
+    EXPECT_EQ(power(-2, -3), -0.125);
+}
+TEST(PowerTest, LargeExponent) {
+    EXPECT_EQ(power(2, 10), 1024.0);
 }
